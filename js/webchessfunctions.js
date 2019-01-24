6ac614ca934d06195ec42740a1ad17e5f1c7d806
rzha1 <44590108+rzha1@users.noreply.github.com>
2019-01-23 20:05:23 -0500

    modified functions, includes alpha beta and evaluations

--- js/webchessfunctions.js
+++ js/webchessfunctions.js
@@ -16,7 +16,6 @@ var minimaxRoot = function (depth, chess, maxplayer) {
         }
     }
     return bestMove;
-
 }
 var evaluateBoard = function (chess) {
     //letters of columns
@@ -28,7 +27,7 @@ var evaluateBoard = function (chess) {
         let column = columns[i];
         for (var k = 0; k < rows.length; k++) {
             let row = rows[k];
-            let value = getPieceValue(chess.get(column + row));
+            let value = getPieceValue(chess.get(column + row),i,k);
             total += value;
         }
     }
@@ -68,27 +67,88 @@ var minimax = (depth, chess, alpha, beta, maxplayer) => {
         return value;
     }
 }
-var getPieceValue = function (piece) {
+var getPieceValue = function (piece,x,y) {
     if (piece === null) {
         return 0;
     }
-    var getAbsoluteValue = function (piece) {
+    var getAbsoluteValue = function (piece,x,y) {
         if (piece.type === 'p') {
-            return 10;
+            return 10 + (piece.color === 'w' ? pawnTable[x][y] : reverseTable(pawnTable)[x][y]);
         } else if (piece.type === 'r') {
-            return 50;
+            return 50 + (piece.color === 'w' ? rookTable[x][y] : reverseTable(rookTable)[x][y]);
         } else if (piece.type === 'n') {
-            return 30;
+            return 30 + (piece.color === 'w' ? knightTable[x][y] : reverseTable(knightTable)[x][y]);
         } else if (piece.type === 'b') {
-            return 30;
+            return 30 + (piece.color === 'w' ? bishopTable[x][y] : reverseTable(bishopTable)[x][y]);
         } else if (piece.type === 'q') {
-            return 90;
+            return 90 + (piece.color === 'w' ? queenTable[x][y] : reverseTable(queenTable)[x][y]);
         } else if (piece.type === 'k') {
-            return 900;
+            return 900 + (piece.color === 'w' ? kingTable[x][y] : reverseTable(kingTable)[x][y]);
         }
         throw "Unknown piece type: " + piece.type;
     };
 
-    var absoluteValue = getAbsoluteValue(piece);
+    var absoluteValue = getAbsoluteValue(piece, x, y);
     return piece.color === 'w' ? absoluteValue : -absoluteValue;
 };
+
+var pawnTable = [
+    [  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 50, 50, 50, 50, 50, 50, 50, 50],
+    [ 10, 10, 20, 30, 30, 20, 10, 10],
+    [  5,  5, 10, 25, 25, 10,  5,  5],
+    [  0,  0,  0, 20, 20,  0,  0,  0],
+    [  5, -5, -10, 0,  0,-10, -5,  5],
+    [  5, 10,  10,-20,-20, 10, 10, 5],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+var knightTable = [[-50, -40, -30, -30, -30, -30, -40, -50],
+[-40, -20, 0, 0, 0, 0, -20, -40],
+[-30, 0, 10, 15, 15, 10, 0, -30],
+[-30, 5, 15, 20, 20, 15, 5, -30],
+[-30, 0, 15, 20, 20, 15, 0, -30],
+[-30, 5, 10, 15, 15, 10, 5, -30],
+[-40, -20, 0, 5, 5, 0, -20, -40],
+    [-50, -40, -30, -30, -30, -30, -40, -50]];
+
+var bishopTable = [[-20, -10, -10, -10, -10, -10, -10, -20],
+[-10, 0, 0, 0, 0, 0, 0, -10],
+[-10, 0, 5, 10, 10, 5, 0, -10],
+[-10, 5, 5, 10, 10, 5, 5, -10],
+[-10, 0, 10, 10, 10, 10, 0, -10],
+[-10, 10, 10, 10, 10, 10, 10, -10],
+[-10, 5, 0, 0, 0, 0, 5, -10],
+    [-20, -10, -10, -10, -10, -10, -10, -20]];
+
+var rookTable = [
+  [0, 0, 0, 0, 0, 0, 0, 0],
+    [5, 10, 10, 10, 10, 10, 10, 5],
+    [-5, 0, 0, 0, 0, 0, 0, -5],
+    [-5, 0, 0, 0, 0, 0, 0, -5],
+    [-5, 0, 0, 0, 0, 0, 0, -5],
+    [-5, 0, 0, 0, 0, 0, 0, -5],
+    [-5, 0, 0, 0, 0, 0, 0, -5],
+   [ 0, 0, 0, 5, 5, 0, 0, 0]];
+
+var queenTable = [[-20, -10, -10, -5, -5, -10, -10, -20],
+[-10, 0, 0, 0, 0, 0, 0, -10],
+[-10, 0, 5, 5, 5, 5, 0, -10],
+[-5, 0, 5, 5, 5, 5, 0, -5],
+   [ 0, 0, 5, 5, 5, 5, 0, -5],
+[-10, 5, 5, 5, 5, 5, 0, -10],
+[-10, 0, 5, 0, 0, 0, 0, -10],
+    [-20, -10, -10, -5, -5, -10, -10, -20]];
+
+var kingTable = [[-30, -40, -40, -50, -50, -40, -40, -30],
+[-30, -40, -40, -50, -50, -40, -40, -30],
+[-30, -40, -40, -50, -50, -40, -40, -30],
+[-30, -40, -40, -50, -50, -40, -40, -30],
+[-20, -30, -30, -40, -40, -30, -30, -20],
+[-10, -20, -20, -20, -20, -20, -20, -10],
+    [20, 20, 0, 0, 0, 0, 20, 20],
+    [20, 30, 10, 0, 0, 10, 30, 20]];
+
+var reverseTable = table => {
+    return table.reverse();
+}
