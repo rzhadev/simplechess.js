d592c3e3296a5fadabfaada4a7d7382b50e7bd8b
rzha1 <44590108+rzha1@users.noreply.github.com>
2019-01-23 22:43:18 -0500

    removed discord stuff

--- commands/webchess.js
+++
@@ -1,6 +0,0 @@
-module.exports.run = async (client, message, args) => {
-    message.channel.send('play me on this https://rzhadev.github.io/smartchess.js/');
-}
-module.exports.help = {
-    name: "webchess",
-}
